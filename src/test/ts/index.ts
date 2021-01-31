febd1c09a98a37e73373b7ae5bf8360aa5afb707
Anton Golub <golub.anton@gmail.com>
2021-01-31 20:08:25 +0300

    feat: add initial gitDir implementation

--- src/test/ts/index.ts
+++ src/test/ts/index.ts
@@ -1,7 +1,37 @@
-import { foo } from '../../main/ts'
+import fs from 'fs-extra'
+import path from 'path'
+import tempy from 'tempy'
 
-describe('index', () => {
-  it('foo() result equals bar', () => {
-    expect(foo()).toBe('bar')
+import { gitDir } from '../../main/ts'
+
+describe('gitDir', () => {
+  it('handles `gitdir: ref` and returns target path if exists', async () => {
+    const temp0 = tempy.directory()
+    const temp1 = tempy.directory()
+    const data = `gitdir: ${temp1}.git `
+
+    await fs.outputFile(path.join(temp0, '.git'), data, {
+      encoding: 'utf8',
+    })
+
+    expect(await gitDir(temp0)).toBe(temp1)
+  })
+
+  it('returns undefined if `gitdir: ref` is unreachable', async () => {
+    const temp = tempy.directory()
+    const data = `gitdir: /foo/bar/baz.git `
+
+    await fs.outputFile(path.join(temp, '.git'), data, { encoding: 'utf8' })
+
+    expect(await gitDir(temp)).toBeUndefined()
+  })
+
+  it('returns undefined if `gitdir: ref` is invalid', async () => {
+    const temp = tempy.directory()
+    const data = `gitdir: broken-ref-format`
+
+    await fs.outputFile(path.join(temp, '.git'), data, { encoding: 'utf8' })
+
+    expect(await gitDir(temp)).toBeUndefined()
   })
 })
