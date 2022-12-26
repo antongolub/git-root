533fad60322212c8288a0625ce85662cef5b2491
Anton Golub <antongolub@antongolub.com>
2022-12-26 21:47:43 +0300

    chore: up deps

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -5,10 +5,10 @@ import util from 'node:util'
 import { Extends, ICallable } from '@qiwi/substrate'
 import findUp, { Match } from 'find-up'
 
-export const isPromiseLike = (value: any): boolean =>
+export const isPromiseLike = (value: unknown): boolean =>
   typeof (value as any)?.then === 'function'
 
-export const effect = <
+export const apply = <
   V extends any, // eslint-disable-line
   C extends ICallable,
   R1 = Promise<ReturnType<C>>,
@@ -23,33 +23,32 @@ export type TGitRootSync = (cwd?: string) => Match
 
 export type TGitRoot = {
   <S>(cwd?: string, sync?: S): Extends<S, boolean, Match, Promise<Match>>
-  sync: TGitRootSync
+  sync?: TGitRootSync
 }
 
-// @ts-ignore
 export const gitRoot: TGitRoot = <S>(
   cwd?: string,
   sync?: S,
 ): Extends<S, boolean, Match, Promise<Match>> => {
-  const exec = sync ? findUp.sync : findUp
+  const find = sync ? findUp.sync : findUp
   const readFile = sync ? fs.readFileSync : util.promisify(fs.readFile)
   const lStat = sync ? fs.lstatSync : util.promisify(fs.lstat)
 
-  return exec(
+  return find(
     (directory) => {
       const gitDir = path.join(directory, '.git')
 
-      return effect(exec.exists(gitDir), (exists) =>
-        !exists
-          ? undefined
-          : effect(lStat(gitDir), (stat) =>
+      return apply(find.exists(gitDir), (exists) =>
+        exists
+          ? apply(lStat(gitDir), (stat) =>
               stat.isDirectory()
                 ? directory
-                : effect(
+                : apply(
                     readFile(gitDir, { encoding: 'utf-8' }),
                     (gitRef) => /^gitdir: (.*)\.git\s*$/.exec(gitRef)?.[1],
                   ),
-            ),
+            )
+          : undefined
       )
     },
     { type: 'directory', cwd },
